3ba78d4480a7324ebd274732f450a12d359d93f5
Aaron Wislang <aaron.wislang@microsoft.com>
2022-11-29 22:14:49 -0500

    sudo make config
    Signed-off-by: Aaron Wislang <aaron.wislang@microsoft.com>

--- linux/vm-mastodon/mastodon.sh
+++ linux/vm-mastodon/mastodon.sh
@@ -8,7 +8,7 @@ git clone https://github.com/asw101/tmp -b fractured-monkey-1
 cd tmp
 
 make run-postgres
-make config
+sudo make config
 sudo make setup-db
 make setup-admin > ../admin.txt
 make run
