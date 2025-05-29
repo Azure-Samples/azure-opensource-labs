7efd71689e37b21da844e9e71fa35a09ac4b14fb
Paul Yu <paul.d.yu@gmail.com>
2025-05-29 10:16:48 -0700

    fix: remove unnecessary comment character from vm_image_offer description

--- cloud-native/aks-byo-models-kaito/workstation/variables.tf
+++ cloud-native/aks-byo-models-kaito/workstation/variables.tf
@@ -13,7 +13,7 @@ variable "vm_size" {
 variable "vm_image_offer" {
   type        = string
   default     = "ubuntu-24_04-lts"
-  description = "publisher of the virtual machine image - look up additional offers with `# az vm image list-offers -l <your_location> -p canonical --query \"[].name\" -o tsv`"
+  description = "publisher of the virtual machine image - look up additional offers with `az vm image list-offers -l <your_location> -p canonical --query \"[].name\" -o tsv`"
 }
 
 variable "vm_username" {
