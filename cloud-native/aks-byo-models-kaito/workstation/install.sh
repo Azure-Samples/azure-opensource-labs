1c755dfd548d6d6b92f7bc36acfef26577f9db35
Paul Yu <paul.d.yu@gmail.com>
2025-05-29 12:26:03 -0700

    fix: reorganize installation steps for kubectl and kubelogin in install.sh

--- cloud-native/aks-byo-models-kaito/workstation/install.sh
+++ cloud-native/aks-byo-models-kaito/workstation/install.sh
@@ -17,6 +17,15 @@ sudo mv kit /usr/local/bin/
 sudo curl -o /usr/local/bin/cog -L "https://github.com/replicate/cog/releases/latest/download/cog_$(uname -s)_$(uname -m)"
 sudo chmod +x /usr/local/bin/cog
 
+# install kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+# install kubelogin
+wget https://github.com/Azure/kubelogin/releases/download/v0.2.7/kubelogin-linux-amd64.zip
+unzip kubelogin-linux-amd64.zip
+sudo mv /bin/linux_amd64/kubelogin /usr/local/bin/kubelogin
+
 # install docker
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
@@ -26,13 +35,4 @@ sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 sudo groupadd docker
 sudo usermod -aG docker "${current_user}"
-exec sg docker "$0"
-
-# install kubectl
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-
-# install kubelogin
-wget https://github.com/Azure/kubelogin/releases/download/v0.2.7/kubelogin-linux-amd64.zip
-unzip kubelogin-linux-amd64.zip
-sudo mv /bin/linux_amd64/kubelogin /usr/local/bin/kubelogin
+newgrp docker
